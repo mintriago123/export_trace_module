7416f48f4b2c3c021200cb63dabc22872d3fb7a1
mintriago123 <michaelintriago1212@gmail.com>
2025-05-10 01:44:14 -0500

    Agregar swagger al proyecto

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
+using Microsoft.OpenApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Export Trace API", Description = "", Version = "v1" });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI(c =>
+   {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "Export Trace API V1");
+   });
+}
 
 app.MapGet("/", () => "Hello World!");
 
